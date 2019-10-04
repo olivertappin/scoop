e4b958e01bf200ced3e8d79ec13ba404331c2786
Oliver Tappin <olivertappin@gmail.com>
2019-10-04 13:42:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Scoop
+
+## Introduction
+
+Scoop was created to solve the problem of shovelling messages from one queue to another, with the option of choosing how
+many messages you want to send.
+
+### Why?
+
+This might be necessary for a number of reasons. The more likely scenario is a disaster situation, where you have a
+multiple node RabbitMQ cluster where one or more node has failed. You have messages on a queue which are not HA, and
+they are building up.
+
+After many '/' vhost errors, and your cluster looking more unhealthy by the minute, you may want
+to rearrange where some of your messages are being distributed to. With the shovel plugin, you have no choice but to
+shovel all your messages. Scoop solves this by giving you the option of how many you want to move around at a time. 
+
+### Tell me more!
+
+It's written in Golang and is distributed as a binary package for all supported platforms and architectures.
+
+## Usage
+
+To use Scoop via the binary, run:
+
+```bash
+bin/scoop -from <from-queue-name> -to <to-queue-name> -count <number-of-messages-to-move>
+```
+
+To run Scoop directly via the source code, run:
+
+```bash
+go run src/scoop.go -from <from-queue-name> -to <to-queue-name> -count <number-of-messages-to-move>
+```
+
+## Arguments
+- `username` - The username used to connect (default: `guest`)
+- `password` - The password used to connect (default: `guest`)
+- `hostname` - The hostname to connect to (default: `localhost`)
+- `port` - The port to connect to (default: `5672`)
+- `from` - The queue name to consume messages from (required)
+- `to` - The queue name to deliver messages to (required)
+- `count` - The _maximum_ number of messages to move (default: `1`)
+- `v` - Turn on verbose mode (default: `false`)
+
+## Notes
+
+Scoop attempts to achieve shovelling with the important option of being able to control how many messages are shovelled into another queue.
+
+Another way of achieving this (suggested by [@EagleEyeJohn](https://github.com/EagleEyeJohn)) would be to create a new queue with the following arguments:
+```
+x-overflow: reject-publish
+x-max-length: <count>
+```
+
+You can then, optionally, create another queue. You would then move your messages out of your bad queue, into this new queue.
+
+Finally, moving the messages into the first queue you created (with the arguments shown above), you can shovel messages into that queue, and they will stop when they get to the `x-max-length` limit.
+
+## Contributing
+
+Please open a GitHub issue for discussion before opening a pull-request.
+
+Once created, create your branch in the following format: `{feature|hotfix}/{github-issue-number}` and open the relevant
+pull-request into the master branch for review.
+
+
+Before pushing your changes, ensure you have compiled the binary file using:
+
+```bash
+go build -o bin/scoop src/scoop.go
+```
+
+All contributions, no matter how large or small, are welcome.
---
+++ src/scoop.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+    "log"
+    "flag"
+    "fmt"
+    "github.com/streadway/amqp"
+)
+
+var (
+    username         = flag.String("username", "guest", "Username")
+    password         = flag.String("password", "guest", "Password")
+    hostname         = flag.String("hostname", "localhost", "Hostname")
+    port             = flag.String("port", "5672", "Port")
+    fromQueueName    = flag.String("from", "", "The queue name to consume messages from")
+    toQueueName      = flag.String("to", "", "The queue name to deliver messages to")
+    exchange         = flag.String("exchange", "", "The exchange name to deliver messages through")
+    messageCount     = flag.Int("count", 1, "The number of messages to move between queues")
+    verbose          = flag.Bool("v", false, "Turn on verbose mode")
+    veryVerbose      = flag.Bool("vv", false, "Turn on very verbose mode")
+    extremelyVerbose = flag.Bool("vvv", false, "Turn on extremely verbose mode")
+)
+
+func init() {
+    flag.Parse()
+}
+
+func main() {
+
+    if (*extremelyVerbose) {
+        log.Printf("Extremely verbose mode enabled")        
+    } else if (*veryVerbose) {
+        log.Printf("Very verbose mode enabled")        
+    } else if (*verbose) {
+        log.Printf("Verbose mode enabled")        
+    }
+
+    // Set the verbose modes accordingly
+    if (*extremelyVerbose) {
+        *veryVerbose = true
+        *verbose = true
+    } else if (*veryVerbose) {
+       *verbose = true   
+    }
+
+    if (*fromQueueName == "") {
+        log.Printf("The from argument must be defined")
+    }
+
+    if (*toQueueName == "") {
+        log.Printf("The to argument must be defined")
+    }
+
+    conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", *username, *password, *hostname, *port))
+    failOnError(err, "Failed to connect to RabbitMQ")
+    defer conn.Close()
+
+    ch, err := conn.Channel()
+    failOnError(err, "Failed to open a channel")
+    defer ch.Close()
+
+    if (*verbose) {
+        log.Printf("Moving %d messages from queue %s to %s", *messageCount, *fromQueueName, *toQueueName)
+    }
+
+    // Check if the queue exists, otherwise, fail
+    // To do this, add the additional argument of passive to true: https://github.com/streadway/amqp/blob/master/channel.go#L758
+    // so if the queue does exist, the command fails (but we need the latest code for that)
+
+    fromQueue, err := ch.QueueDeclare(
+        *fromQueueName, // name
+        false,          // durable
+        false,          // delete when unused
+        false,          // exclusive
+        false,          // no-wait
+        nil,            // arguments
+    )
+    failOnError(err, "Failed to declare a queue")
+
+    if (*veryVerbose) {
+        log.Printf("There are %d messages in queue %s", fromQueue.Messages, fromQueue.Name)
+    }
+
+    toQueue, err := ch.QueueDeclare(
+        *toQueueName, // name
+        false,        // durable
+        false,        // delete when unused
+        false,        // exclusive
+        false,        // no-wait
+        nil,          // arguments
+    )
+    failOnError(err, "Failed to declare a queue")
+
+    if (*veryVerbose) {
+        log.Printf("There are %d messages in queue %s", toQueue.Messages, toQueue.Name)
+    }
+
+    msgs, err := ch.Consume(
+        fromQueue.Name, // queue
+        "",             // consumer
+        false,          // auto-ack (it's very important this stays false)
+        false,          // exclusive
+        false,          // no-local
+        false,          // no-wait
+        nil,            // args
+    )
+    failOnError(err, "Failed to register the scoop consumer")
+
+    log.Printf("Running scoop consumer... (press Ctl-C to cancel)")
+
+    i := 1
+
+    for d := range msgs {
+        if (i > *messageCount) {
+            if (*extremelyVerbose) {
+                log.Printf("Complete")
+            }
+            break
+        }
+
+        err = ch.Publish(
+            *exchange,    // exchange
+            toQueue.Name, // routing key
+            false,        // mandatory
+            false,        // immediate
+            amqp.Publishing{
+                ContentType: "text/plain",
+                Body:        []byte(d.Body),
+            })
+
+        failOnError(err, "Failed to deliver message")
+
+        if (*extremelyVerbose) {
+            log.Printf("Successfully delivered message (%d/%d)", i, *messageCount)
+        }
+
+        d.Ack(true)
+        i++
+    }
+}
+
+func failOnError(err error, msg string) {
+    if err != nil {
+        log.Fatalf("%s: %s", msg, err)
+    }
+}
